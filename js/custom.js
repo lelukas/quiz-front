0828dc06064e5892714a172774e7fa649ca340a4
lelukas <lelukass@gmail.com>
2017-05-03 22:46:05 -0300

    views integrated

--- js/custom.js
+++ js/custom.js
@@ -1,71 +1,108 @@
 $.get("https://appquiz-api.herokuapp.com/category", function (data) {
-    console.log()
-
-    data.forEach(function () {
-        console.log(name)
-    });
-    // $(".ui.grid:nth-child(1)").append()
-});
-
-
-$.get("https://appquiz-api.herokuapp.com/api/quiz/start", function (data) {
-    console.log(data);
-
-    var q = 0;
-    var alt = 0;
-    var score = 0;
-
-    // console.log(data);
-
-    function list() {
-        $(".question").html(data.questions[q].question);
-
-        $(".alternative").each(function () {
-            $(this).html(data.questions[q].alternatives[alt]);
-            alt++;
-        });
+    for(var i=0; i<5; i++){
+        $(".ui.grid:nth-child(1)").append(
+            `<div class="game category button column" id-value="${data[i].id}">
+                <p class="category name"> ${data[i].name} </p>
+             </div>`
+        );
+        // console.log(data[i].name)
     }
 
-    list();
+    $(".ui.grid:nth-child(1)").append(
+        "<div class='game category button column' id-value='0'>" +
+            "<p class='all category name'>Todas</p>" +
+        "</div>"
+    );
 
+    var c = 0;
+    var t = data.length;
+    setInterval(function () {
+        $(".all").html(data[c].name);
+        c++;
+        if(c == t) c = 0;
 
-    $(".game-button").click(function () {
-        $(".selected").removeClass("selected");
-        $(this).addClass("selected");
-        $(".answer").removeAttr("disabled");
-    });
-
-
-    $(".answer").click(function () {
-
-        $(this).attr("disabled","disabled");
-
-        if($(".selected").attr("value") == data.questions[q].correct){
-            score++;
-            console.log(score);
+        if(Math.random() <= 0.3){
+            $(".all").html("Todas");
+        }else if(Math.random() <= 0.1){
+            $(".all").html("Você conhece sobre tudo?");
         }
+    },200);
 
-        $(".selected").removeClass("selected");
+    $("div.category.button").click(function () {
+        // console.log($(this).attr("id-value"));
 
-        alt = 0;
-        q++;
+        if($(this).attr("id-value") != 0)
+            var catId = $(this).attr("id-value");
+        else
+            var catId = "";
 
-        if(q == 5){
+        $(".ui.grid:nth-child(1)").addClass("hide");
+        $(".ui.grid:nth-child(2)").removeClass("hide");
 
-            $(".ui.grid:nth-child(1)").addClass("hide");
-            $(".ui.grid:nth-child(2)").removeClass("hide");
-            $(".score").html("Você acertou " + score + " perguntas");
+        // console.log("https://appquiz-api.herokuapp.com/api/quiz/start" + catId);
 
+        $.get("https://appquiz-api.herokuapp.com/api/quiz/start/" + catId, function (data) {
 
-            $.post("https://appquiz-api.herokuapp.com/api/answer", {
-                name: "Leo",
-                score: score
-            })
+            // console.log("https://appquiz-api.herokuapp.com/api/quiz/" + $(this).attr("id-value"));
+            // console.log(data);
 
-        }else{
+            var q = 0;
+            var alt = 0;
+            var score = 0;
+
+            // console.log(data);
+
+            function list() {
+                $(".question").html(data.questions[q].question);
+
+                $(".alternative").each(function () {
+                    $(this).html(data.questions[q].alternatives[alt]);
+                    alt++;
+                });
+            }
+
             list();
-        }
-    })
+
+
+            $(".game.button").click(function () {
+                $(".selected").removeClass("selected");
+                $(this).addClass("selected");
+                $(".answer").removeAttr("disabled");
+            });
+
+
+            $(".answer").click(function () {
+
+                $(this).attr("disabled","disabled");
+
+                if($(".selected").attr("value") == data.questions[q].correct){
+                    score++;
+                    console.log(score);
+                }
+
+                $(".selected").removeClass("selected");
+
+                alt = 0;
+                q++;
+
+                if(q == 5 || data.questions[q] == null){
+
+                    $(".ui.grid:nth-child(2)").addClass("hide");
+                    $(".ui.grid:nth-child(3)").removeClass("hide");
+                    $(".score").html("Você acertou " + score + " perguntas");
+
+                }else{
+                    list();
+                }
+            })
+        });
+    });
 });
+
+
+
+//https://appquiz-api.herokuapp.com/api/quiz/start/oId
+
+
 
 
